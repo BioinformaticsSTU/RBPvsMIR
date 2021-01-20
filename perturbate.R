4f53865c909e03f87e7efe5b3b3c0180bfda1870
Bioinformatics Department, Shantou University Medical College <48581957+BioinformaticsSTU@users.noreply.github.com>
2021-01-20 16:11:43 +0800

    Update perturbate.R

--- perturbate.R
+++ perturbate.R
@@ -1,18 +1,16 @@
-
-
-#STRING="atcgatcga";
 args<-commandArgs(T);
-dir_name=args[1];
+TITLE=args[1];
 STRING=args[2];
 
 STRING=toupper(STRING);
+dir_name=TITLE+"_dir";
 dir.create(dir_name);
 
 for(i in 1:999){
 	Sequence=strsplit(STRING, split="")[[1]];
 	Sequence_p=sample(Sequence, length(Sequence));
 	STRING_p=paste(Sequence_p, collapse="");
-	file_name=paste(c(dir_name, '_dir/string', i, '.txt'), collapse="");
+	file_name=paste(c(dir_name, '/string', i, '.txt'), collapse="");
 	#print(file_name);
 	cat(">SEQUENCE\n", file=file_name);
 	cat(STRING_p, file=file_name, append=TRUE);
